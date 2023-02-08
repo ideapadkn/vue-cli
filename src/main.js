b83c10f1f8867997aef95befdb629f665e752c09
Kamron or Камрон <kamrush6429@gmail.com>
2023-02-08 13:32:08 +0500

    optimize menu and date

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import 'materialize-css/dist/js/materialize.min.js'
+import 'materialize-css/dist/js/materialize.min.js';
+import dateFilter from "./filters/date.filter";
+
+// Vue.filter('date', dateFilter)
 
 createApp(App).use(store).use(router).mount("#app");
